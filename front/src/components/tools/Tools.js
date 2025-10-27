98d24a0779eee9d41cb6ce9c2188d07a71a531d8
aniosmain <135284964+aniosmain@users.noreply.github.com>
2025-10-26 22:02:52 -0400

    Remove unused imports

--- front/src/components/tools/Tools.js
+++ front/src/components/tools/Tools.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import './Tools.scss';
 import Zip2Terr from "./Zip2Terr";
 import BlurbGenerator from "./BlurbGenerator";
